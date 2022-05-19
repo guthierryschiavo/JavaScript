da21fd26406a67935d7e8d7abff58ecb672f99fa
Guthierry Schiavo Lima <98849784+guthierryschiavo@users.noreply.github.com>
2022-05-19 18:55:16 -0300

    Exercícios JavaScript
    Exercícios do Guanabara

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+var idade = 17
+
+if ( idade < 18){
+    console.log('Você está abaixo da idade')
+} else {
+    if (idade == 18){
+        console.log('você está na idade permitida')
+    } else {
+        console.log('Você está acima da idade')
+    }
+}
